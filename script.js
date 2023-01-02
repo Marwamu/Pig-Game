d2a78fd5bd07cd6e05484b41cb318003f62d1493
Marwamu <marwamohamed1293@gmail.com>
2023-01-02 11:21:28 +0200

    create hold and newgame functions

--- script.js
+++ script.js
@@ -4,8 +4,31 @@ var holdBtn = document.getElementById('holdBtn');
 var diceImg = document.getElementById('diceImg');
 var current0 = document.getElementById('current--0')
 var current1 = document.getElementById('current--1')
+var player1 = document.getElementById('player1')
+var player2 = document.getElementById('player2')
+var score0 = document.getElementById('score--0')
+var score1 = document.getElementById('score--1')
+var name0 = document.getElementById('name--0')
+var name1 = document.getElementById('name--1')
+var player1score = 0
+var player2score = 0
 var switcher = true;
+var gameover = false;
+const resetGame = () => {
+    current0.innerHTML = 0;
+    score0.innerHTML = 0;
+    current1.innerHTML = 0;
+    score1.innerHTML = 0;
+    player1.classList.add('player--active');
+    switcher = true;
+    name0.innerHTML = 'Player 1';
+    name1.innerHTML = 'Player 2';
+    gameover = false;
+}
 rollDiceBtn.addEventListener('click', () => {
+    if (gameover) {
+        return
+    }
     // var random = Math.random();
     min = Math.ceil(1);
     max = Math.floor(6);
@@ -15,30 +38,64 @@ rollDiceBtn.addEventListener('click', () => {
     var y = parseInt(current1.innerHTML);
     if (switcher) {
         if (number == 1) {
+            player1.classList.remove('player--active');
+            player2.classList.add('player--active');
             current0.innerHTML = 0;
             switcher = false;
         }
         else {
             x += number;
             current0.innerHTML = x;
+            if (x + player1score >= 100) {
+                name0.innerHTML = 'Winner !';
+                gameover = true;
+                return;
+            }
+
         }
     }
     else {
         if (number == 1) {
+            player2.classList.remove('player--active');
+            player1.classList.add('player--active');
             current1.innerHTML = 0;
             switcher = true;
         }
         else {
             y += number;
             current1.innerHTML = y;
+            if (y + player2score >= 100) {
+                name1.innerHTML = 'Winner !';
+                gameover = true;
+                return;
+            }
         }
     }
     // console.log(object);
 
 })
-newGameBtn.addEventListener('click', () => {
+newGameBtn.addEventListener('click', resetGame)
 
-})
+
 holdBtn.addEventListener('click', () => {
-    
+    if (gameover) {
+        return
+    }
+    if (switcher) {
+        player1score += parseInt(current0.innerHTML);
+        score0.innerHTML = player1score;
+        switcher = false;
+        player1.classList.remove('player--active');
+        player2.classList.add('player--active');
+        current0.innerHTML = 0;
+    }
+    else {
+        player2score += parseInt(current1.innerHTML);
+        score1.innerHTML = player2score;
+        switcher = true;
+        player2.classList.remove('player--active');
+        player1.classList.add('player--active');
+        current1.innerHTML = 0;
+    }
+
 })
